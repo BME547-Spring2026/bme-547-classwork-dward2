4b8698080491e97aae9c5e88ef48a5ebd16f4628
David Ward <daw74@duke.edu>
2026-01-15 15:05:58 -0500

    Driver function for HDL analysis written with link from main menu

--- blood_analysis.py
+++ blood_analysis.py
@@ -3,10 +3,13 @@ def interface():
     keep_running = True
     while keep_running:
         print("Options:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == "9":
             keep_running = False
+        elif choice == "1":
+            HDL_driver()
 
 def input_HDL_value():
     choice = input("Enter the HDL result: ")
@@ -25,4 +28,9 @@ def output_HDL_result(hdl_result):
     print("The result of the HDL test is {}".format(hdl_result))
     return
     
+def HDL_driver():
+    hdl_input = input_HDL_value()
+    hdl_result = check_HDL(hdl_input)
+    output_HDL_result(hdl_result)
+
 interface()
