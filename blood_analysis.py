a17d0c825275e64cf505cf1f025a6f857898e326
David Ward <daw74@duke.edu>
2026-01-14 21:51:20 -0500

    Generalized the driver and the analysis

--- blood_analysis.py
+++ blood_analysis.py
@@ -10,52 +10,39 @@ def interface():
         if choice == "9":
             keep_running = False
         elif choice == "1":
-            HDL_driver()
+            generic_driver("HDL")
         elif choice == "2":
-            LDL_driver()
+            generic_driver("LDL")
     return
     
 def input_test_value(test_name):
     choice = input("Enter the {} result: ".format(test_name))
     return choice
     
-def check_HDL(hdl_input):
-    hdl_value = int(hdl_input)
-    if hdl_value >= 60:
-        return "Normal"
-    elif 40 <= hdl_value < 60:
-        return "Borderline Low"
-    elif hdl_value < 40:
-        return "Low"
+def get_test_ranges(test_name):
+    if test_name == "HDL":
+        ranges = ((0, "Low"), (40, "Borderline Low"),(60, "Normal"))
+    elif test_name == "LDL":
+        ranges = ((0, "Normal"), (130, "Borderline High"), (160, "High"), (190, "Very High"))
+    return ranges
+
+def generic_test_analysis(test_ranges, test_result):
+    test_value = int(test_result)
+    for i in range(len(test_ranges)-1):
+        if test_ranges[i][0] <= test_value < test_ranges[i+1][0]:
+            return test_ranges[i][1]
+    return test_ranges[-1][1]
         
 def output_test_result(test_name, test_value, test_result):
     print("The result of {} for the {} test is {}".format(test_value, test_name, test_result))
     return
     
-def HDL_driver():
-    test_name = "HDL"
+def generic_driver(test_name):
     test_input = input_test_value(test_name)
-    test_result = check_HDL(test_input)
+    test_ranges = get_test_ranges(test_name)
+    test_result = generic_test_analysis(test_ranges, test_input)
     output_test_result(test_input, test_name, test_result)
-    
-def check_LDL(ldl_input):
-    ldl_value = int(ldl_input)
-    if ldl_value < 130:
-        return "Normal"
-    elif 130 <= ldl_value < 160:
-        return "Borderline High"
-    elif 160 <= ldl_value < 190:
-        return "High"
-    elif ldl_value >= 190:
-        return "Very High"
         
-def LDL_driver():
-    test_name = "LDL"
-    test_input = input_test_value(test_name)
-    test_result = check_LDL(test_input)
-    output_test_result(test_input, test_name, test_result)
-    
-    
 interface()
 
     
