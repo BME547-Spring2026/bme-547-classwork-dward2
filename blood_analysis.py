e0ed7ddedc996f7f22fab79ae4d0c71f28ffa0c5
David Ward <daw74@duke.edu>
2026-01-14 21:34:05 -0500

    Implemented LDL test analysis

--- blood_analysis.py
+++ blood_analysis.py
@@ -4,12 +4,15 @@ def interface():
         print("My Program")
         print("Options:")
         print("1-HDL")
+        print("2-LDL")
         print("9-Quit")
         choice = input("Enter your choice: ")
         if choice == "9":
             keep_running = False
         elif choice == "1":
             HDL_driver()
+        elif choice == "2":
+            LDL_driver()
     return
     
 def input_HDL_value():
@@ -34,6 +37,29 @@ def HDL_driver():
     hdl_result = check_HDL(hdl_input)
     output_HDL_result(hdl_result)
     
+def input_LDL_value():
+    choice = input("Enter the LDL result: ")
+    return choice
+    
+def check_LDL(ldl_input):
+    ldl_value = int(ldl_input)
+    if ldl_value < 130:
+        return "Normal"
+    elif 130 <= ldl_value < 160:
+        return "Borderline High"
+    elif 160 <= ldl_value < 190:
+        return "High"
+    elif ldl_value >= 190:
+        return "Very High"
+        
+def output_LDL_result(ldl_result):
+    print("The result of the LDL test is {}".format(ldl_result))
+    return
+    
+def LDL_driver():
+    ldl_input = input_LDL_value()
+    ldl_result = check_LDL(ldl_input)
+    output_LDL_result(ldl_result)
     
     
 interface()
