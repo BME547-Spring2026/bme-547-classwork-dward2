78a91b8746fdb20fe7d0492c21a3588ecff1ea75
David Ward <daw74@duke.edu>
2026-01-15 15:02:38 -0500

    Characterize entered HDL value as Normal, borderline low, or low

--- blood_analysis.py
+++ blood_analysis.py
@@ -11,6 +11,14 @@ def interface():
 def input_HDL_value():
     choice = input("Enter the HDL result: ")
     return choice
-        
+    
+def check_HDL(hdl_input):
+    hdl_value = int(hdl_input)
+    if hdl_value >= 60:
+        return "Normal"
+    elif 40 <= hdl_value < 60:
+        return "Borderline Low"
+    elif hdl_value < 40:
+        return "Low"
         
 interface()
