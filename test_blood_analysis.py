59a1d1007fb8c2646c2e31a33ce25371d65321a8
David Ward <daw74@duke.edu>
2026-01-22 18:44:43 -0500

    Finished PEP-8 fixes

--- test_blood_analysis.py
+++ test_blood_analysis.py
@@ -1,10 +1,11 @@
 import pytest
 
-@pytest.mark.parametrize("hdl_value, expected",
-    [(65, "Normal"),
+
+@pytest.mark.parametrize("hdl_value, expected", [
+     (65, "Normal"),
      (45, "Borderline Low"),
      (20, "Low")
-     ])   
+     ])
 def test_check_HDL(hdl_value, expected):
     # arranges
     from blood_analysis import check_HDL
@@ -12,19 +13,19 @@ def test_check_HDL(hdl_value, expected):
     answer = check_HDL(hdl_value)
     # asserts
     assert answer == expected
-    
 
-@pytest.mark.parametrize("ldl_value, expected",
-        [(100, "Normal"),
-         (135, "Borderline High"),
-         (165, "High"),
-         (200, "Very High")])
+
+@pytest.mark.parametrize("ldl_value, expected", [
+    (100, "Normal"),
+    (135, "Borderline High"),
+    (165, "High"),
+    (200, "Very High")])
 def test_check_LDL(ldl_value, expected):
     from blood_analysis import check_LDL
     answer = check_LDL(ldl_value)
     assert answer == expected
-    
-    
+
+
 """
 conda activate envname
 
