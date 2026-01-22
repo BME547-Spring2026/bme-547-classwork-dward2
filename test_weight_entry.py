81e1fe7e8916bf8480feaa7fa1b53ecdea7f6243
David Ward <daw74@duke.edu>
2026-01-22 18:40:59 -0500

    Start robust unit testing exercise

---
+++ test_weight_entry.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.mark.parametrize("weight_input, expected",[
+    ("22 lb", 10),
+    ("50 kg", 50)])
+def test_parse_weight_input(weight_input, expected):
+    from weight_entry import parse_weight_input
+    answer = parse_weight_input(weight_input)
+    assert answer == expected
+    
+    
