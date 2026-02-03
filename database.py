74769d8fa697a8df698d1dad5295e27b438f81b9
David Ward <daw74@duke.edu>
2026-02-03 12:17:47 -0500

    Process each line of input file to create a patient dictionary

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
+"""
+
+    patient = {"first name": <first_name>, "last name": <last_name>", "mrn": <mrn_int>, "age": <age_int>}
+   
+* Write create_patient that receives one input line (example: "Ann Ables, 123, 34\n") and 
+  returns a patient dictionary
+* Write another function that iterates through the input (patient_all) and calls create_patient for each patient 
+  and prints patient dictionary
+
+"""
+
+
 def load_patient_file():
     # Example using open/close
     in_file = open("patient_data.txt", "r")
@@ -13,12 +25,26 @@ def load_patient_file():
         patient_all = in_file.readlines()
     print(patient_all)
 
-    return
+    return patient_all
+    
+def create_patient(line):
+    line = line.strip("\n")
+    data = line.split(",")
+    first_name, last_name = data[0].split(" ")
+    patient = {"first_name": first_name, "last_name": last_name,
+               "mrn": int(data[1]), "age": int(data[2])}
+    return patient
+
+
+def process_all_patients(patient_raw_data):
+    for item in patient_raw_data:
+        patient = create_patient(item)
+        print(patient)
 
 
 def main():
-    load_patient_file()
-
+    patient_raw_data = load_patient_file()
+    process_all_patients(patient_raw_data)
 
 if __name__ == "__main__":
     main()
